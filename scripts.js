46ffd053453d284b8a8c27639c0d813c1af2787e
Zeknir <Zeknir@users.noreply.github.com>
2015-09-10 18:47:56 +0200

    1.1 Update
    Added Style
    Added Elements
    Fixed selectable Button

--- scripts.js
+++ scripts.js
@@ -1,38 +1,66 @@
-﻿var environents = ["Overworld", "Underground", "Water", "Ghost House", "Airship", "Castle"];
-var objects = ["Blocks", "Mushrooms", "Coins", "Springboards", "Goombas", "Koopas", "Piranha Plants", "Feathers", "Propeller Mushrooms", "Tanookis", "Lakitus", "Spinys", "Moving platforms",
+﻿var style = ["Super Mario Bros", "Super Mario Bros 3", "Super Mario World", "New Super Mario Bros U"];
+var environents = ["Overworld", "Underground", "Water", "Ghost House", "Airship", "Castle"];
+
+var objects = ["Blocks", "Mushrooms", "Coins", "Springboards", "Goombas", "Koopas", "Piranha Plants", "Lakitus", "Clouds", "Spinys", "Moving platforms",
                "Fireflowers", "Stars", "Bullet Bills", "Bill Blasters", "Bloopers", "Cheep-cheep", "Hammer Bros", "Buzzy Beetles", "Thwomps", "Podoboos", "Bowser", "Bowser Jr.",
-               "Noteblocks", "Skull platforms", "Firebars", "Rocky Wrenches (Moles)", "Bob-ombs", "Donut Blocks", "POW-Blocks", "Firethrowers", "Cannons", "Spike Tops", "Boos", "Dry Bones",
-               "Kameks", "Hidden blocks", "Munchers", "Wigglers", "Clowncars", "Chain Chomps"];
-var mechanic = ["wall-jumping", "ground-pounding", "pipes", "costumes", "platforms", "vines", "doors", "spikes", "Yoshis", "Kuribo's Shoe", "rails", "conveyor belts", "sawblades", "ice blocks",
-                "oneway blocks"];
-var additionalMechanic = ["scrolling", "fast scrolling", "slow scrolling", "short time", "sound effects"];
+               "Noteblocks", "Musicblocks", "Skull platforms", "Firebars", "Rocky Wrenches", "Monty Moles", "Bob-ombs", "Donut Blocks", "POW-Blocks", "Firethrowers", "Cannons", "Spike Tops", "Boos", "Dry Bones",
+               "Kameks", "Hidden blocks", "Munchers", "Wigglers", "Clowncars", "Chain Chomps", "Spiny helmet", "Beetle helmet"];
+var nsmbuOnlyObjects = ["Propeller Mushrooms"];
+var smwOnlyObjects = ["Feathers"];
+var smb3OnlyObjects = ["Tanookis"];
+var smbOnlyObjects = [""];
+
+var mechanics = ["Pipes", "Platforms", "Vines", "Doors", "Spikes", "Yoshis", "Kuribo's Shoe", "Rails", "Conveyor belts", "Sawblades", "Ice blocks",
+                "Oneway blocks"];
+var nsmbuOnlyMechanics = ["Wall-jumping", "Ground-pounding", "Spin-jumping"];
+var smwOnlyMechanics = ["Spin-jumping"];
+var smbOnlyMechanics = ["Costumes"];
+
+var additionalMechanics = ["Scrolling", "Fast scrolling", "Slow scrolling", "Short time", "Sound effects", "Mazes", "Puzzles"];
 
 function generate() {
-    var outputElement = document.getElementById("ideaText");
+
+    var outputStyleElement = document.getElementById("ideaStyle");
+    var styleId = getRandom(0, style.length - 1);
+    outputStyleElement.innerHTML = getStyle(styleId);
+
+    var outputTypeElement = document.getElementById("ideaType");
+    outputTypeElement.innerHTML = getEnvironment();
+
+    var outputContentElement = document.getElementById("ideaContent");
     var outputText = "";
 
-    outputText = outputText.concat(getEnvironment());
-    outputText = outputText.concat(" with ");
-
-    var objectCount = getRandom(1, 2);
+    var objectCount = getRandom(1, 3);
     for (var i = 0; i < objectCount; i++) {
         if (getRandom(1, 2) === 1) {
-            outputText = outputText.concat(getObject());
+            outputText = outputText.concat(getObject(styleId));
         } else {
-            outputText = outputText.concat(getMechanic());
+            outputText = outputText.concat(getMechanic(styleId));
         }
 
-        if (i < objectCount - 1) {
+
+
+        if (i === objectCount - 2) {
             outputText = outputText.concat(" and ");
+        }
+        else if (i < objectCount - 1) {
+            outputText = outputText.concat(", ");
         }
     }
 
-    if (getRandom(1, 6) === 1) {
+    if (getRandom(1, 2) === 1) {
         outputText = outputText.concat(" and ");
         outputText = outputText.concat(getAdditionalMechanic());
     }
 
-    outputElement.innerHTML = outputText;
+    outputContentElement.innerHTML = outputText;
+}
+
+function getStyle(styleId) {
+    var output = "";
+    output = output.concat(style[styleId]);
+    output = output.concat(" style");
+    return output;
 }
 
 function getEnvironment() {
@@ -42,10 +70,23 @@ function getEnvironment() {
     return output;
 }
 
-function getObject() {
+function getObject(styleId) {
     var output = "";
 
-    output = output.concat(objects[getRandom(0, objects.length - 1)]);
+    if (styleId === 0) {
+        allObjects = objects.concat(smbOnlyObjects);
+    }
+    else if (styleId === 1) {
+        allObjects = objects.concat(smb3OnlyObjects);
+    }
+    else if (styleId === 2) {
+        allObjects = objects.concat(smwOnlyObjects);
+    }
+    else if (styleId === 3) {
+        allObjects = objects.concat(nsmbuOnlyObjects);
+    }
+
+    output = output.concat(allObjects[getRandom(0, allObjects.length - 1)]);
 
     if (getRandom(1, 2) === 1) {
         if (getRandom(1, 3) === 1) {
@@ -58,15 +99,30 @@ function getObject() {
     return output;
 }
 
-function getMechanic() {
+function getMechanic(styleId) {
     var output = "";
-    output = mechanic[getRandom(0, mechanic.length - 1)];
+    var allMechanics;
+
+    if (styleId === 0) {
+        allMechanics = mechanics.concat(smbOnlyMechanics);
+    }
+    else if (styleId === 2) {
+        allMechanics = mechanics.concat(smwOnlyMechanics);
+    }
+    else if (styleId === 3) {
+        allMechanics = mechanics.concat(nsmbuOnlyMechanics);
+    }
+    else {
+        allMechanics = mechanics;
+    }
+
+    output = allMechanics[getRandom(0, allMechanics.length - 1)];
     return output;
 }
 
 function getAdditionalMechanic() {
     var output = "";
-    output = additionalMechanic[getRandom(0, additionalMechanic.length - 1)];
+    output = additionalMechanics[getRandom(0, additionalMechanics.length - 1)];
     return output;
 }
 
